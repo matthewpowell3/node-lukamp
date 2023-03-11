24a97de85f8da77c77ec0890ebec87017f35c530
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-11 17:59:56 -0500

    2 files modified, 1 added

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const http = require('http');
-const fs = require('fs');
-const ytdl = require('youtube-dl');
+const express = require('express');
+const app = express();
+const ytdl = require('ytdl-core');
 
-const server = http.createServer((req, res) => {
-  // Serve the HTML file when the client requests the root URL
-  if (req.url === '/') {
-    fs.readFile('index.html', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
-        res.end('Error loading index.html');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  }
+// Serve HTML page
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+// Handle video download request
+app.get('/download', function (req, res) {
+  var url = req.query.url;
+  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  ytdl(url, { format: 'mp4' }).pipe(res);
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
 });
