e811cc59e781f3498103bb5f53e473cb85fe5804
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-11 17:19:33 -0500

    1 file modified, 1 added, 1 deleted

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // Serve the HTML file when the client requests the root URL
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
